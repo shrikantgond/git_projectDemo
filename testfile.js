d8e9c1a32a2682e45772173b0e3faf0ff4e84cad
shrikantg <shrikantg@winjit.com>
2020-12-07 12:50:39 +0530

    my miletone Fixes #18

--- testfile.js
+++ testfile.js
@@ -2,5 +2,5 @@ function myfunc() {
     console.log('Hi')
 //todo #12 I will create issue here @shrikantgond
 //todo #13 done with this@shrikantgond
-// I dont think I can work with this keyboard I will need to return it.
+// I dont think I can work with this keyboard I will need to return it. 
 }
