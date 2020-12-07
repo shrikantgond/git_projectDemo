3d9be2e5c7dc7a7339b99acfc016e356367fead6
shrikantg <shrikantg@winjit.com>
2020-12-07 18:38:39 +0530

    klhkkbkbdf Fixes #24

--- testfile.js
+++ testfile.js
@@ -1,6 +1,3 @@
 function myfunc() {
     console.log('Hi')
-//todo #12 I will create issue here @shrikantgond
-//todo #13 done with this@shrikantgond
-// I dont think I can work with this keyboard I will need to return it. 
 }
